ec91353d83bd9fe66fdfdf218e8d11e4d2947c95
surbhir2024 <surbhi.rank.2024@gmail.com>
2024-03-27 17:55:37 +0530

    task9

--- routers/allrouters.js
+++ routers/allrouters.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 require('dotenv').config();
 const routes = express.Router();
-// const connection = require('../connection.js')
+const connection = require('../connection.js')
 const path = require('path');
 const { stat } = require('fs/promises');
 
@@ -32,5 +32,154 @@ routes.get('/awan',(req,res)=>{
 })
 routes.get('/hirex',(req,res)=>{
     res.render('task8/hirex');
+})
+
+
+let search = function(res,data,page,max_page,fileds){
+    res.render('task9/search',{
+        data: data,
+        crr_page: +page,
+        last_page : max_page,
+        fileds:fileds
+    })
+}
+
+routes.get('/homegreedsearch',(req,res)=>{
+    res.render('task9/home')
+})
+
+routes.get('/form',(req,res)=>{
+
+    let url = req.url;
+    let query = req.query.query;
+    let page = req.query.page;
+
+    if (query) {
+    url = url.split("?");
+    console.log(url);
+    let newQuery = query;
+    let length12;
+    if (query.indexOf("LIMIT")) newQuery = query.substring(0, query.length - 11);
+    console.log(newQuery);
+
+    //newQuery += "Limit 10,10" 
+
+    connection.query(newQuery,function(err,newResult,fileds){
+        if(err) throw err;
+        else{
+            length12 = newResult.length;
+            // console.log(newResult);
+            console.log(length12);
+
+        }
+
+        res.render('task9/dynamic',{
+            data : newResult,
+            fileds : fileds,
+            query : newQuery,
+            crr_page : +page,
+            crr_page_url : url[0]
+        });
+    });
+
+    }else{
+        res.render('task9/dynamic',{fileds:false,data:false,query:false});
+    }
+
+})
+
+
+routes.post('/form',(req,res)=>{
+    const query = req.body.query;
+    let page = req.query.page || 1;
+    let url = req.url;
+    connection.query(query,(err,data,fileds)=>{
+        if(err){
+            res.json(({error:err.sqlMessage}))
+        }
+        
+        else
+            res.render('task9/dynamic',{
+            data : data,
+            fileds : fileds,
+            query : query,
+            crr_page : page,
+            crr_page_url : url,
+            datalength : data.length
+           
+        });
+        // console.log(data.length);
+        console.log(url);
+        
+    })
+})
+
+routes.get('/')
+
+
+//searching routes
+routes .get('/search',(req,res)=>{
+    records = process.env.Max_Record;
+    page = req.query.page || 1;
+    max_page = process.env.max_page;
+    sql = `select * from Student_master LIMIT ${records * (page - 1)},${records}`;
+    let query = connection.query(sql,(err,data,fileds)=>{
+        if(err) throw err;
+        res.render('task9/search',{
+            data: data,
+            crr_page: +page,
+            last_page : max_page,
+            fileds:fileds
+        })
+        // console.log(data);  
+    })    
+})
+
+
+
+routes.post('/search',(req,res)=>{
+    records = process.env.Max_Record;
+    page = req.query.page || 1;
+    max_page = process.env.max_page;
+    id = req.body.stu_id;
+    Firstname = req.body.fname;
+    Lastname = req.body.lname;
+    city = req.body.city;
+    state1 = req.body.state;
+    dob = req.body.dob;
+    
+
+    let sql;
+    if(id != undefined && id != "")
+    {
+        sql = `select * from Student_master where student_id = ${id} LIMIT ${records * (page - 1)},${records}`;
+        connection.query(sql,(err,data,fileds)=>{
+            if(err) throw err;
+            search(res,data,page,max_page,fileds);
+            //  console.log(data);  
+        })  
+    }
+    else if(req?.body?.SearchMore == "Search"){
+        dob1 = dob.split(',');
+    
+        sql = `select * from Student_master where (FirstName like "${Firstname}%" and LastName = "${Lastname}" and City = "${city}" and State = "${state1}") 
+        or (DOB BETWEEN "${dob1[0]}" and "${dob1[1]}") ORDER BY DOB LIMIT ${records * (page - 1)},${records}`;
+        connection.query(sql,(err,data,fileds)=>{
+            if(err) throw err;
+            search(res,data,page,max_page,fileds);
+            //  console.log(data);  
+        })  
+         
+    }
+    else{
+        res.render('task9/search',{
+            data: false,
+
+        })
+        //  console.log(data);  
+    }
+        //  console.log(data);  
+      
+    
 })
 module.exports = routes;
